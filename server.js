359693da4297b5e61851a888fc031b724b3e05cf
Eric Tovar <tovareric@gmail.com>
2019-07-11 17:52:02 -0500

    server set up

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use('/api/users', require('./routes/api/users'));
 // Put API routes here, before the "catch all" route
 
 // The following "catch all" route (note the *)is necessary
@@ -24,7 +25,7 @@ app.get('/*', function(req, res) {
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`)
