50f1e3932a12d50ba789bb0bdb762ab37f512e96
Eric Tovar <tovareric@gmail.com>
2019-07-12 15:29:29 -0400

    server set up

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 
 const app = express();
 
-require('dotenv').config({ path: 'ENV_FILENAME' });
+require('dotenv').config();
 require('./config/database');
 
 app.use(logger('dev'));
