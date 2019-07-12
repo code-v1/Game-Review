43211401d80d38a44a96978c8c1b3218f7fc57dc
Eric Tovar <tovareric@gmail.com>
2019-07-12 15:27:55 -0400

    navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import {Nav, NavLink, NavItem} from 'reactstrap';
 import './NavBar.css';
+
+
 
 const NavBar = () => {
   return (
-    <div className='NavBar'>
-      <Link to="/login" className='NavBar-link'>LOG IN</Link>
-      &nbsp;&nbsp;|&nbsp;&nbsp;
-      <Link to="/signup" className='NavBar-link'>SIGN UP</Link>
-    </div>
-  );
-};
+    <div>
+        <Nav tabs>
+            <NavLink href="#">My Account</NavLink>
+            <NavLink href="#">Games</NavLink>
+            <NavItem className='login'>
+            <NavLink href="#">Sign In</NavLink> 
+            </NavItem>
+            <NavItem className='login'>
+            <NavLink href="#">Log In</NavLink> 
+            </NavItem>
+        </Nav>
+      </div>
+    );
+  }
+
+//     <div className='NavBar'>
+//       <Link to="/login" className='NavBar-link'>LOG IN</Link>
+//       &nbsp;&nbsp;|&nbsp;&nbsp;
+//       <Link to="/signup" className='NavBar-link'>SIGN UP</Link>
+//     </div>
+//   );
+// };
 
 export default NavBar;
