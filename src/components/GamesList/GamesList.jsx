bce4f26f56a1da0928d1dd97d8d4e16a9a6917ce
Eric Tovar <tovareric@gmail.com>
2019-07-14 23:33:24 -0500

    component to show games list

---
+++ src/components/GamesList/GamesList.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class GamesList extends Component {
+    render()
+{
+            return (
+            <div>
+                <p>Show Game list </p>
+            </div>
+        )
+};
+};
