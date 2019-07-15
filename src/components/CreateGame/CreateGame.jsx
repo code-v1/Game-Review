2e311043fda2f84d7cde1c199a8adc072dc97752
Eric Tovar <tovareric@gmail.com>
2019-07-14 23:34:11 -0500

    component to create game

---
+++ src/components/CreateGame/CreateGame.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class CreateGame extends Component {
+    render()
+{
+            return (
+            <div>
+                <p>Create Game Review </p>
+            </div>
+        )
+};
+};
