3f83c5e1e58a9c265863f605224758d16f9f0f31
Eric Tovar <tovareric@gmail.com>
2019-07-17 10:45:23 -0500

    adds log

--- controllers/users.js
+++ controllers/users.js
@@ -15,6 +15,7 @@ async function signup(req, res) {
     const token = createJWT(user);
     res.json({ token });
   } catch (err) {
+    console.log('signup', err)
     // Probably a duplicate email
     res.status(400).json(err);
   }
